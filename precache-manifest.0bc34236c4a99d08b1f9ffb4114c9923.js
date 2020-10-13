98eb1b0be47f472d141522b59a695a226e6ae5f5
yunyshs01 <jeoung09@naver.com>
2020-10-13 21:45:50 +0900

    Updates

---
+++ precache-manifest.0bc34236c4a99d08b1f9ffb4114c9923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd7055b301698ecc128eb5676439b25",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "e0009228b0ae9a8a566e",
+    "url": "/nwitter/static/css/main.ef774b5e.chunk.css"
+  },
+  {
+    "revision": "3f5504b1cd3b223d711b",
+    "url": "/nwitter/static/js/2.4a26cce2.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/nwitter/static/js/2.4a26cce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0009228b0ae9a8a566e",
+    "url": "/nwitter/static/js/main.d6db9a22.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46ec67cf0d3542a1a229553eeeecd365.js"
+  "/nwitter/precache-manifest.0bc34236c4a99d08b1f9ffb4114c9923.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nwitter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
