bbd3481d0b8c23b00ed9a0316a169a9c5281d753
yunyshs01 <jeoung09@naver.com>
2020-10-13 21:43:12 +0900

    Updates

---
+++ precache-manifest.0d4fe465c3b32f316543a980b65ba61b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c913ee61f61204661e21156c8d44f9",
+    "url": "/yunyshs01/nwitter.git/index.html"
+  },
+  {
+    "revision": "e0009228b0ae9a8a566e",
+    "url": "/yunyshs01/nwitter.git/static/css/main.ef774b5e.chunk.css"
+  },
+  {
+    "revision": "ef0634c68c398fc09743",
+    "url": "/yunyshs01/nwitter.git/static/js/2.eb7c53af.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/yunyshs01/nwitter.git/static/js/2.eb7c53af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0009228b0ae9a8a566e",
+    "url": "/yunyshs01/nwitter.git/static/js/main.d6db9a22.chunk.js"
+  },
+  {
+    "revision": "90b388c3fde5800061f4",
+    "url": "/yunyshs01/nwitter.git/static/js/runtime-main.a5db06f6.js"
+  }
+]);
